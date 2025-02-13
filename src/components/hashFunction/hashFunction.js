cfb9cdefceaf1bab860e29df5c2afb12550fff27
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-02-14 00:08:01 +0700

    Fix hash function

--- src/components/hashFunction/hashFunction.js
+++ src/components/hashFunction/hashFunction.js
@@ -1,18 +1,11 @@
+import CryptoJS from 'crypto-js';
+
 export const hashFunction = async (inputString) => {
+  if (!inputString) return null;
   try {
-    // Create UTF-8 encoded array from input string
-    const encoder = new TextEncoder();
-    const data = encoder.encode(inputString);
-
-    // Generate SHA-256 hash
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-
-    // Convert buffer to hex string
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join('');
-
+    const sha256Hash = CryptoJS.SHA256(inputString).toString();
     // Return first 15 characters
-    return hashHex.substring(0, 15);
+    return sha256Hash.substring(0, 15);
   } catch (error) {
     console.error('Error generating hash:', error);
     return null;
