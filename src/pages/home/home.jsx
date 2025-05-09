8aa12687ec02014f1219c0087153bc90b2390f96
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-05-09 15:35:58 +0700

    styles: change style home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,83 +1,64 @@
-import React from 'react';
-import { useRef } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import React, { useEffect, useState } from 'react';
+import HomePage from '/HomePage.png';
 import './home.scss';
 
-// Import images
-import img1 from '../../assets/images/instructions/1.png';
-import img3 from '../../assets/images/instructions/3.png';
-import img4 from '../../assets/images/instructions/4.png';
-import img5 from '../../assets/images/instructions/5.png';
-import img6 from '../../assets/images/instructions/6.png';
-import img7 from '../../assets/images/instructions/7.png';
-import img8 from '../../assets/images/instructions/8.png';
-import img9 from '../../assets/images/instructions/9.png';
-import img10 from '../../assets/images/instructions/10.png';
-import img11 from '../../assets/images/instructions/11.png';
-import img12 from '../../assets/images/instructions/12.png';
-import img13 from '../../assets/images/instructions/13.png';
-import img14 from '../../assets/images/instructions/14.png';
-import img15 from '../../assets/images/instructions/15.png';
-import img16 from '../../assets/images/instructions/16.png';
-import img17 from '../../assets/images/instructions/17.png';
-
-const slideImages = [
-  img1,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13,
-  img14,
-  img15,
-  img16,
-  img17,
-];
-
 const Home = () => {
-  const sliderRef = useRef(null);
+  const [trail, setTrail] = useState(Array.from({ length: 10 }, () => ({ x: 0, y: 0 })));
 
-  // Auto-play functionality
+  // Cập nhật vị trí chuột và tạo hiệu ứng đuôi sao băng
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      setTrail((prevTrail) => {
+        const newTrail = [...prevTrail];
+        newTrail.unshift({ x: clientX, y: clientY });
+        newTrail.pop();
+        return newTrail;
+      });
+    };
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true, // Disable default arrows
-  };
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
   return (
-    <div className='home-container'>
-      <div className='iframe-container'>
-        <iframe src='https://docs.google.com/spreadsheets/d/e/2PACX-1vRvtSLcMtb3ERriz9uh3AxKuf2uwxP5QW79EXxezZwFBqOraziQ2faf18T_T77ooRhggdscwKDgkSFx/pubhtml?gid=742653863&amp;single=true&amp;widget=true&amp;headers=false'></iframe>
+    <>
+      {trail.map((pos, index) => (
+        <React.Fragment key={index}>
+          <div
+            className='trail-shadow'
+            style={{
+              left: `${pos.x}px`,
+              top: `${pos.y}px`,
+              opacity: (1 - index / trail.length) * 0.5,
+            }}
+          />
+          {/* Đuôi sáng phía trên */}
+          <div
+            className='trail'
+            style={{
+              left: `${pos.x}px`,
+              top: `${pos.y}px`,
+              opacity: 1 - index / trail.length,
+            }}
+          />
+        </React.Fragment>
+      ))}
+
+      <div
+        className='home'
+        style={{
+          backgroundImage: `url(${HomePage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          minHeight: '100vh',
+          width: '100%',
+        }}
+      >
+        {/* Nội dung của bạn ở đây */}
       </div>
-      <h1 className='home-title'>Quy trình nhận và nhập sách!</h1>
-      <Slider ref={sliderRef} {...settings}>
-        {slideImages.map((img, index) => (
-          <div key={index} className='home-slide'>
-            <img src={img} alt={`Slide ${index + 1}`} className='home-slide-image' />
-          </div>
-        ))}
-      </Slider>
-      <div className='home-controls'>
-        <button onClick={() => sliderRef.current.slickPrev()} className='control-button'>
-          Trước
-        </button>
-        <button onClick={() => sliderRef.current.slickNext()} className='control-button'>
-          Sau
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
